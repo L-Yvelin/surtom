9a8e74fb9a9c25a1f4039a389a15ca31b2900c23
L-Yvelin <louisyvelin34720@gmail.com>
2026-04-16 19:52:17 +0200

    feat: Player XP back

--- back/src/services/databaseService.ts
+++ back/src/services/databaseService.ts
@@ -464,9 +464,30 @@ class DatabaseService {
   }
 
   async getPlayerXp(playerName: string): Promise<number> {
-    return new Promise((resolve) => {
-      resolve(0);
-    });
+    type PlayerXpRow = {
+      XP: number;
+    };
+
+    const [rows] = await this.pool.query<(PlayerXpRow & RowDataPacket)[]>(
+      `
+    SELECT 
+      COALESCE(SUM(
+        CASE
+          WHEN t.Win = 1 THEN 35 - POW(t.AttemptCount - 1, 2)
+          ELSE 5
+        END
+      ), 0) AS XP
+    FROM Try t
+    WHERE t.PlayerID = (
+      SELECT p.ID
+      FROM Player p
+      WHERE p.Username = ?
+    );
+    `,
+      [playerName],
+    );
+
+    return rows[0]?.XP ?? 0;
   }
 }
 
