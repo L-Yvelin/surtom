bae3db558126f1d2a9b3857265d70dbd1826262f
L-Yvelin <louisyvelin34720@gmail.com>
2026-04-16 17:14:39 +0200

    feat: Preload Surtom Studios loading image

--- front/src/components/Loading/Loading.tsx
+++ front/src/components/Loading/Loading.tsx
@@ -10,7 +10,7 @@ function Loading({ display }: LoadingProps): JSX.Element {
   return (
     <div className={classNames(classes.loading, { [classes.hidden]: !display })}>
       <div className={classes.brand}>
-        <img src="/assets/surtom_studios.png" alt="Mojang logo" className={classes.logo} />
+        <img src="/assets/surtom_studios.png" alt="Surtom Studios logo" className={classes.logo} />
       </div>
       <div className={classes.loadingBar}>
         <div className={classes.loadingProgress}></div>
