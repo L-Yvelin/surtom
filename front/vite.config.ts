b95f904889c61fed5d801734ba3f6a25414ac721
L-Yvelin <louisyvelin34720@gmail.com>
2026-05-13 17:43:27 +0200

    build: Use React 19 compiler

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+    svgr(),
+  ],
   server: {
     port: 27021,
   },
